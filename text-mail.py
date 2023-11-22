c4f3555d5b0e7c430e0b905d25624ab0edbdee82
Imoh Henry <henryimoh2@gmail.com>
2023-11-23 00:11:41 +0100

    Add files via upload

---
+++ text-mail.py
@@ -0,0 +1,28 @@
+import smtplib
+import getpass
+
+HOST = "smtp.mail.yahoo.com"
+PORT = 587
+
+FROM_EMAIL = "henry_imoh@yahoo.com"
+TO_EMAIL = "henryimoh2@gmail.com"
+PASSWORD = getpass.getpass("Enter password: ")
+
+MESSAGE = """Subject: test mail pls
+this is purely a test mail, I hope this works
+
+"""
+
+smtp = smtplib.SMTP(HOST, PORT)
+
+status_code, response = smtp.ehlo()
+print(f"[*] Echoing the server: {status_code} {response}")
+
+status_code, response = smtp.starttls()
+print(f"[*] Starting TLS connection: {status_code} {response}")
+
+status_code, response = smtp.login(FROM_EMAIL, PASSWORD)
+print(f"[*] Logging in: {status_code} {response}")
+
+smtp.sendmail(FROM_EMAIL, TO_EMAIL, MESSAGE)
+smtp.quit()
